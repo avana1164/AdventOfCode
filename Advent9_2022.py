4223e6a6610d4e2b012de9f80504b896cece8bab
AAnand05 <44912634+fligmiger@users.noreply.github.com>
2022-12-17 15:24:35 -0500

    removed unnecessary if statements

--- Advent9_2022.py
+++ Advent9_2022.py
@@ -2,21 +2,14 @@ file = open('adventfile9_2022', 'r')
 f = file.readlines()
 v = []
 coor = {0: [0, 0], 1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0], 6: [0, 0], 7: [0, 0], 8: [0, 0], 9: [0, 0]}
+dir = {'R': [1, 0], 'L': [-1, 0], 'U': [0, 1], 'D': [0, -1]}
 for line in f:
     s = line.split()
-    dir = s[0]
+    m = s[0]
     moves = int(s[1])
     for _ in range(moves):                    
-        if dir == 'R':              
-            coor[0][0]+=1 
-        elif dir == 'L':  
-            coor[0][0]-=1           
-        elif dir == 'U':          
-            coor[0][1]+=1                      
-        elif dir == 'D':  
-            coor[0][1]-=1 
-
-       
+        coor[0][0]+=dir[m][0]
+        coor[0][1]+=dir[m][1]
         for i in range(9):                     
             y = coor[i][1] - coor[i + 1][1]
             x = coor[i][0] - coor[i + 1][0]
@@ -33,14 +26,15 @@ for line in f:
                 elif x < 0 and y < 0:
                     coor[i + 1][0] -= 1
                     coor[i + 1][1] -= 1
-                elif x > 1:
+                elif x > 0:
                     coor[i + 1][0] += 1
-                elif x < -1:
+                elif x < 0:
                     coor[i + 1][0] -= 1
-                elif y > 1:
+                elif y > 0:
                     coor[i + 1][1] += 1
-                elif y < -1:
-                    coor[i + 1][1] -= 1 
+                elif y < 0:
+                    coor[i + 1][1] -= 1
+
             if tuple(coor[9]) not in v:
                 v.append(tuple(coor[9]))                                  
 print(len(v))      
