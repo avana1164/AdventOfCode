0a81597f61e27d7d3101375e08f876857a03324f
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-11-28 18:15:51 -0500

    Optimized solution, removed AA from nonzero

--- 2022/d16/d16.py
+++ 2022/d16/d16.py
@@ -3,6 +3,7 @@ f = file.readlines()
 paths = {}
 valves = {}
 visited = {}
+dist = {}
 nonzero = []
 
 for line in f:
@@ -15,14 +16,14 @@ for line in f:
     if int(rate) > 0:
         nonzero.append(line[1])
         valves[line[1]] = int(rate)
+        dist[line[1]] = {}
         
     paths[line[1]] = connections
     visited[line[1]] = [0, False]
 
-nonzero.insert(0, "AA")
+dist["AA"] = {}
 
-dist = [[float('inf')]* len(nonzero) for _ in range(len(nonzero))]
-for node in nonzero:
+for node in dist:
     visited = {k: [0, False] for k in visited}
     queue = []
     queue.append(node)
@@ -34,24 +35,33 @@ for node in nonzero:
                 visited[v][1] = True
                 visited[v][0] = visited[u][0] + 1
                 queue.append(v)
-                if v in nonzero:
-                    dist[nonzero.index(node)][nonzero.index(v)] = visited[v][0]
+                if v in dist and v != "AA":
+                    dist[node][v] = visited[v][0]
         queue.pop(0)
-        
+
+indices = {}
+for i in range(len(nonzero)):
+    indices[nonzero[i]] = i
+ 
 cache = {}
-    
+
+# Try to use dictionaries instead of arrays to make lookup times faster
 def max_pressure(current, time, opened):
     if (current, time, opened) in cache:
         return cache[(current, time, opened)]
         
     max_val = 0
 
-    for next in nonzero:
-        remtime = time - dist[nonzero.index(current)][nonzero.index(next)] - 1
-        bit = 1 << nonzero.index(next)
-        if not (opened & bit) and remtime >= 0 and next != "AA":
-            pressure = valves[next]*(remtime)
-            max_val = max(max_val, pressure + max_pressure(next, remtime, opened | bit))    
+    for next in dist[current]:
+        remtime = time - dist[current][next] - 1
+        #adding continues instead of one if statement makes code slightly faster
+        if remtime < 0:
+            continue
+        bit = 1 << indices[next]
+        if opened & bit:
+            continue
+        pressure = valves[next]*(remtime)
+        max_val = max(max_val, pressure + max_pressure(next, remtime, opened | bit))    
     
     cache[(current, time, opened)] = max_val
     
@@ -60,6 +70,7 @@ def max_pressure(current, time, opened):
 # part 1 
 # print(max_pressure("AA", 30, 0)) 
 
+# part 2 (don't add AA to non zero, an extra bit means twice the number of iterations)
 mask = (1 << len(nonzero)) - 1
 max_val = 0
 
