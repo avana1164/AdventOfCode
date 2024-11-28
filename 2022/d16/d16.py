ae4d26775fba716b1297a6d1a9e0f748f86f11f9
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-11-28 14:59:27 -0500

    part 2 works now

--- 2022/d16/d16.py
+++ 2022/d16/d16.py
@@ -1,8 +1,9 @@
 file = open('d16', 'r')
 f = file.readlines()
+paths = {}
 valves = {}
-nodes = []
-nodes_of_interest = []
+visited = {}
+nonzero = []
 
 for line in f:
     line = line.strip().split(" ")
@@ -11,55 +12,55 @@ for line in f:
         connections.append(line[i].replace(",", ""))
     
     rate = line[4].replace("rate=", "").replace(";", "")
-    nodes.append(line[1])
     if int(rate) > 0:
-        nodes_of_interest.append(line[1])
+        nonzero.append(line[1])
+        valves[line[1]] = int(rate)
         
-    valves[line[1]] = [int(rate), connections]
+    paths[line[1]] = connections
+    visited[line[1]] = [0, False]
 
+nonzero.insert(0, "AA")
 
-def floyd_warshall(valves, nodes):
-    n = len(nodes)
-    dist = [[float('inf')]*n for _ in range(n)]
-
-    for u in nodes:
-        for v in valves[u][1]:
-            if v in nodes:
-                dist[nodes.index(u)][nodes.index(v)] = 1
-        dist[nodes.index(u)][nodes.index(u)] = 0
-    
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-                
-    return dist
-
-dist = floyd_warshall(valves, nodes)  
-
+dist = [[float('inf')]* len(nonzero) for _ in range(len(nonzero))]
+for node in nonzero:
+    visited = {k: [0, False] for k in visited}
+    queue = []
+    queue.append(node)
+    visited[node][1] = True
+    while len(queue) > 0:
+        u = queue[0]
+        for v in paths[u]:
+            if not visited[v][1]:
+                visited[v][1] = True
+                visited[v][0] = visited[u][0] + 1
+                queue.append(v)
+                if v in nonzero:
+                    dist[nonzero.index(node)][nonzero.index(v)] = visited[v][0]
+        queue.pop(0)
+        
 cache = {}
     
-def max_pressure(current, time, visited):
-    if (current, time, visited) in cache:
-        return cache[(current, time, visited)]
+def max_pressure(current, time, opened):
+    if (current, time, opened) in cache:
+        return cache[(current, time, opened)]
         
     max_val = 0
+
+    for next in nonzero:
+        remtime = time - dist[nonzero.index(current)][nonzero.index(next)] - 1
+        bit = 1 << nonzero.index(next)
+        if not (opened & bit) and remtime >= 0 and next != "AA":
+            pressure = valves[next]*(remtime)
+            max_val = max(max_val, pressure + max_pressure(next, remtime, opened | bit))    
     
-    if time <= 0:
-        return 0
-    
-    for next in nodes_of_interest:
-        distance = dist[nodes.index(current)][nodes.index(next)] + 1
-        if not (visited & (1 << nodes.index(next))) and time - distance >= 0:
-            pressure = valves[next][0]*(time - distance)
-            max_val = max(max_val, pressure + max_pressure(next, time - distance, visited | (1 << nodes.index(next))))
-            cache[(current, time, visited)] = max_val
+    cache[(current, time, opened)] = max_val
     
     return max_val 
-     
-# print(max_pressure("AA", 30, 0)) part 1
+    
+# part 1 
+# print(max_pressure("AA", 30, 0)) 
 
-mask = (1 << len(nodes_of_interest)) - 1
+mask = (1 << len(nonzero)) - 1
 max_val = 0
 
 for i in range((mask + 1)//2):
